63ce8235b7faeeb01c9dddbd3e7329326d0aa664
fshdn1221 <eragonhd1221@gmail.com>
2015-09-21 15:07:14 +0700

    Commit create source folder and project for android client-side

---
+++ 01. Source/client/LolGuideApp/src/com/creative/lolguideapp/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.creative.lolguideapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.creative.lolguideapp.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
