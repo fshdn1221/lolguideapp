745041cfbd854320bb64785694de48c6b86ac53b
vuthanhict <vuthanhict@gmail.com>
2015-09-22 16:26:10 +0700

    Updated SplashScreen: used AsynTask and Handler

--- 01. Source/client/src/com/creative/lolwikia/activity/main/SplashScreen.java
+++ 01. Source/client/src/com/creative/lolwikia/activity/main/SplashScreen.java
@@ -4,6 +4,7 @@ import com.creative.lolwikia.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
@@ -18,7 +19,9 @@ import android.view.Window;
  */
 public class SplashScreen extends Activity {
 
-    private static final long SPLASH_TIME_OUT = 3000L;
+    public static final String TAG = "SplashScreen";
+    public static final long SPLASH_TIME_OUT = 3000L;
+    private boolean flagDataLoaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +29,73 @@ public class SplashScreen extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.screen_splash);
 
+        // Load data from server store into database
+        new LoadData().execute();
+
         new Handler().postDelayed(new Runnable() {
-            
+
             @Override
             public void run() {
-                // Start main activity
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(intent);
-                
-                // Close this activity
-                finish();
+                if (!flagDataLoaded) {
+                    flagDataLoaded = true;
+                } else {
+                    startMainScreen();
+                }
             }
         }, SPLASH_TIME_OUT);
+    }
+
+    /**
+     * Start main activity
+     */
+    private void startMainScreen() {
+        // Start main activity
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+
+        // Close this activity
+        finish();
+    }
+
+    /**
+     * AsyncTask handle load data from server
+     * 
+     * @author ThanhVV
+     *
+     */
+    private class LoadData extends AsyncTask<Void, Void, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            Thread thread = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(4000L);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            thread.start();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            if (!flagDataLoaded) {
+                flagDataLoaded = true;
+            } else {
+                startMainScreen();
+            }
+        }
     }
 }
